c5c4a8dc4c9084c05a9d8125cc519a4f01e57a12
Harsha Lakmal <harshauwu@gmail.com>
2023-10-14 15:32:49 +0530

    update dags and fb details

--- Q2/assignment-backend/controllers/post_controller.py
+++ Q2/assignment-backend/controllers/post_controller.py
@@ -1,9 +1,9 @@
 from flask import request, jsonify
 import praw
 from praw.models import InlineGif, InlineImage, InlineVideo
-from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT, REDDIT_USER_NAME, REDDIT_PASSWORD
+from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT, REDDIT_USER_NAME, REDDIT_PASSWORD, PAGE_ACCESS_TOKEN, PAGE_ID
 from services.post_service import save_post, get_all_schedule_posts
-
+import requests
 
 def get_schedule_posts():
     try:
@@ -18,7 +18,6 @@ def get_schedule_posts():
 
 def create_schedule_post():
     try:
-         
         # Get post data from the form
         data = request.get_json()
         new_post = save_post(data)
@@ -26,7 +25,7 @@ def create_schedule_post():
         if new_post.id:
             if data['is_one_time']:
                 data = create_reddit_post(data)
-                # create_facebook_post(data)
+                create_facebook_post(data)
         return jsonify({"message": "schedule post insert successful", "id": new_post.id, "data": data}), 200
     except Exception as e:
             # Handle exceptions (e.g., database connection error)
@@ -60,5 +59,35 @@ def create_reddit_post(data):
             return f"Error fetching Reddit posts: {str(e)}"
 
 def create_facebook_post(data):
-     print('this is facebook api~')
+    try: 
+
+        # Your Facebook App ID
+        FACEBOOK_PAGE_ID = PAGE_ID
+
+        # Your Facebook Page Access Token
+        FACEBOOK_PAGE_ACCESS_TOKEN = PAGE_ACCESS_TOKEN
+
+        # The Facebook Graph API endpoint for publishing posts
+        FACEBOOK_GRAPH_API_ENDPOINT = f'https://graph.facebook.com/v18.0/{FACEBOOK_PAGE_ID}/feed'
+
+         # Create a POST request to the Facebook Graph API endpoint
+        request = requests.post(
+            FACEBOOK_GRAPH_API_ENDPOINT,
+            headers={
+                'Authorization': 'Bearer {}'.format(FACEBOOK_PAGE_ACCESS_TOKEN)
+            },
+            data={
+                'message': data['text']
+            }
+        )
+
+        # Check if the request was successful
+        if request.status_code == 200:
+            return True
+        else:
+            return False
+        
+    except Exception as e:
+            # Handle exceptions (e.g., database connection error)
+            return f"Error fetching Reddit posts: {str(e)}"
          
